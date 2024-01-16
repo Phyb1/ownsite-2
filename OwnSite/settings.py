d5a091e79618782bd54a4e576a15926a977e5610
Phybeon <85971263+Phyb1@users.noreply.github.com>
2024-01-16 09:35:07 +0200

    Update settings.py

--- OwnSite/settings.py
+++ OwnSite/settings.py
@@ -130,3 +130,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# configuring for ssl
+SECURE_SSL_REDIRECT = True 
+
+CSRF_COOKIE_SECURE = True
+
+SESSION_COOKIE_SECURE = True
